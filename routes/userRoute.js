5cb6357a074a36d7f865cfcd0e3405f7a5322138
KirtimaanVishwakarma <vishwakarma.kirtimaan@gmail.com>
2023-11-03 20:33:00 +0530

    added get user profile

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,11 +3,14 @@ import {
   createUser,
   loginUser,
   logout,
+  userProfile,
 } from "../controllers/userContorller.js";
+import { isAuthenticatedUser } from "../utils/auth.js";
 
 const router = express.Router();
 
 router.route("/register").post(createUser);
+router.route("/profile").get(isAuthenticatedUser, userProfile);
 router.route("/login").post(loginUser);
 router.route("/logout").post(logout);
 // .get(getAllCars);
